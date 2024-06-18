cc847bfdd05557bb7b891252a9ff0ff695f43a7b
Taylor Isbell <taylor.isbell@databricks.com>
2024-06-17 23:36:45 -0500

    forgot message content

--- rag_app.py
+++ rag_app.py
@@ -1,33 +1,28 @@
-import time
-from typing import List
+from typing import Dict, List
 
 import chainlit as cl
 from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
-from langchain.memory import ChatMessageHistory, ConversationBufferMemory
-from langchain_core.output_parsers import StrOutputParser
+from numpy import source
 
-from chain import model, vector_search_as_retriever, with_message_history
+from chain import with_message_history
 
 welcome_message = """
     Welcome to Taylor's Lakehouse Apps demo! Ask anything about
     documents you vectorized and stored in your Databricks Vector Search Index.
 """
-# message_history = ChatMessageHistory()
 
-# memory = ConversationBufferMemory(
-#     memory_key="chat_history",
-#     output_key="answer",
-#     chat_memory=message_history,
-#     return_messages=True,
-# )
 
-# chain = ConversationalRetrievalChain.from_llm(
-#     model,
-#     chain_type="stuff",
-#     retriever=vector_search_as_retriever,
-#     memory=memory,
-#     return_source_documents=True,
-# )
+def string_to_dict_list(formatted_string: str) -> List[Dict[str, str]]:
+    """Convert formatted string to a list of dictionaries."""
+    docs = []
+    for doc_string in formatted_string.strip().split("</context>"):
+        if len(doc_string) == 0:
+            continue
+        doc_dict = {}
+        doc_dict["doc_url"] = doc_string.split("Doc URL: ")[1].split("Doc Snippet: ")[0]
+        doc_dict["doc_snippet"] = doc_string.split("Doc Snippet: ")[-1]
+        docs.append(doc_dict)
+    return docs
 
 
 @cl.on_chat_start
@@ -44,28 +39,17 @@ async def main(message: cl.Message):
     chain = cl.user_session.get("chain")  # type: ConversationalRetrievalChain
     config = cl.user_session.get("config")
 
-    cb = cl.AsyncLangchainCallbackHandler(stream_final_answer=True)
-
-    # res = await chain.acall(message.content, callbacks=[cb])
-
     msg = cl.Message(content="")
 
-    # chunks = []
-    # async for chunk in chain.astream(
-    #     {"input": message.content}, config=config,
-    # ):
-    #     chunks.append(chunk)
-    #     await msg.stream_token(str(chunk))
-
     chunks = {"context": None, "answer": ""}
-    async for chunk in chain.astream({"input": "what is dlt"}, config):
+    async for chunk in chain.astream({"input": message.content}, config):
         if "context" in chunk.keys():
             chunks["context"] = chunk["context"]
             continue
         await msg.stream_token(chunk["answer"])
         chunks["answer"] += chunk["answer"]
 
-    source_documents = chunks["context"]
+    source_documents = string_to_dict_list(chunks["context"])
 
     text_elements = []  # type: List[cl.Text]
 
@@ -73,18 +57,17 @@ async def main(message: cl.Message):
         return s[:100] if len(s) > 100 else s
 
     if source_documents:
-        for source_doc in source_documents.split("</context>"):
-            if len(source_doc)==0:
+        for source_doc in source_documents:
+            if len(source_doc) == 0:
                 continue
             # Create the text element referenced in the message
             text_elements.append(
                 cl.Text(
-                    content=source_doc,
+                    content=truncate(source_doc["doc_snippet"]),
                     # name=(truncate(source_doc.page_content) + " ..."),
-                    name="blah",
+                    name=source_doc["doc_url"],
                     # url=source_doc.metadata["url"],
                 )
             )
     msg.elements = text_elements
     await msg.send()
-
