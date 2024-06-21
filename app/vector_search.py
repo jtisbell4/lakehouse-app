2dc6451f723d2797653303f42b754c48a25e6cdf
Taylor Isbell <taylor.isbell@databricks.com>
2024-06-21 11:54:36 -0400

    more env vars

--- app/vector_search.py
+++ app/vector_search.py
@@ -8,6 +8,7 @@ VECTOR_SEARCH_INDEX = "field_demos.ssc_rag_chatbot.databricks_documentation_vs_i
 
 CLIENT_ID = os.environ.get("DATABRICKS_CLIENT_ID")
 CLIENT_SECRET = os.environ.get("DATABRICKS_CLIENT_SECRET")
+WORKSPACE_URL = os.environ.get("DATABRICKS_HOST")
 
 
 vs_client = VectorSearchClient(
